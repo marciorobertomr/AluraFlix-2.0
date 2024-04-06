32b2a46ccc2db7dcee19878179af2fd5c65dadfb
marciorobertomr <marcio_mr1@hotmail.com>
2024-04-06 14:04:28 -0300

    Atualização/Revisão de Código

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/* function adicionarFilme() {
-    var filmeFavorito = document.getElementById("filme").value;
-    
-    if (filmeFavorito.endsWith(".jpg")) { //verifica se o final de uma string termina com .jpg, identifcando se é uma imagem
-        var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
-        var elementoListaFilmes = document.getElementById("listaFilmes");
-        elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
-    } else {
-        console.error("Endereço de filme inválido");
-    }
-
-    document.getElementById("filme").value = ""; //Limpa o valor do campo input, retornando vazio
-    
-} */
-
 /* LISTA DE FILMES PARA TESTE
     https://br.web.img2.acsta.net/medias/nmedia/18/89/39/66/20044639.jpg - Fúria de Titans 2
     https://upload.wikimedia.org/wikipedia/pt/2/28/Transformers_The_Last_Knight.jpg - Transformers 5
@@ -22,30 +7,20 @@
     https://br.web.img3.acsta.net/pictures/22/04/15/22/05/1348210.jpg - Jurassic World 3: Domínio
     https://http2.mlstatic.com/D_NQ_NP_924548-MLB47711012599_092021-O.jpg - O Hobbit: A Desolação de Smaug
     https://cinema10.com.br/upload/upload/avatar%20cartaz.jpg - Avatar
+    https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/08/25/one-piece-netflix-1jefhfx050m02.jpg
+    One Piece
+    https://images.justwatch.com/backdrop/312101817/s640/avatar-the-last-airbender.%7Bformat%7D.jpg
+    The Last Airbender
 */
-
-/*  DESAFIOS
-    Criar um botão para remover um filme na tela
-    Além de colocar a imagem do filme, também adicionar o nome por meio de outro input
-    Guardar todos os filmes adicionados em uma lista/array e percorrer essa lista toda vez que quiser imprimir ou remover o filme
-    Conversor de moedas: criar funções para cada tipo de conversão e chamá-las dependendo do que você quiser fazer, podendo colocar inputs ou botões e quando clicar chamar cada função converteDolar(), converteReal(), converteEuro() e converteBitcoin(), por exemplo
-*/
-
-
-//Quebra de código em partes menores 
-
-var listaImagem = [];
-var listaNome = [];
-var localLista = "";
 
 function adicionarFilme() {
-    var imagemFilmeFavorito = document.getElementById("imagemFilme").value;
-    var nomeFilmeFavorito = document.getElementById("nomeFilme").value;
+    let imagemFilmeFavorito = document.getElementById("imagemFilme").value;
+    let nomeFilmeFavorito = document.getElementById("nomeFilme").value;
     
-    var novo = true;
+    let novo = true;
     
     if (imagemFilmeFavorito.endsWith(".jpg") && nomeFilmeFavorito != "") { //verifica se o final de uma string termina com .jpg, identifcando se é uma imagem. != verifica se é diferente
-        for (var teste = 0; teste < listaImagem.length; teste++) {
+        for (let teste = 0; teste < listaImagem.length; teste++) {
             if (imagemFilmeFavorito == listaImagem[teste] && nomeFilmeFavorito == listaNome[teste]) {
                 alert("Filme já existente na coletânia.");
                 teste = listaImagem.length;
@@ -76,30 +51,30 @@ function adicionarFilme() {
 }
 
 function listarFilmesNaTela (imagemFilme , nomeFilme , localLista) { //Recebe a variável filmeFavorito na função e adiciona na nova variável filme
-    var elementoFilmeFavorito = "<figure id=" + localLista + "><img src=" + imagemFilme + ">" + "<figcaption>" + nomeFilme + "</figcaption></figure>";
-    var elementoListaFilmes = document.getElementById("listaFilmes");
+    let elementoFilmeFavorito = "<figure id=" + localLista + "><img src=" + imagemFilme + ">" + "<figcaption>" + nomeFilme + "</figcaption></figure>";
+    let elementoListaFilmes = document.getElementById("listaFilmes");
     elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
 }
 
 function retirarFilmesDaTela (localLista) {
-    var filmeDeletado = document.getElementById(localLista); //Pega todo o campo html com o ID informado
+    let filmeDeletado = document.getElementById(localLista); //Pega todo o campo html com o ID informado
     filmeDeletado.remove(); //Deleta o campo html completo
 }
 
 function removerFilme() {
-    var imagemFilmeDeletado = document.getElementById("imagemFilme").value;
-    var nomeFilmeDeletado = document.getElementById("nomeFilme").value;
+    let imagemFilmeDeletado = document.getElementById("imagemFilme").value;
+    let nomeFilmeDeletado = document.getElementById("nomeFilme").value;
 
     if (imagemFilmeDeletado.endsWith(".jpg") && nomeFilmeDeletado != "") {
-        var confirma = confirm("Deseja excluir o filme?");
+        let confirma = confirm("Deseja excluir o filme?");
         if (confirma == true) {
-            for (var i = 0; i < listaImagem.length; i++) {
+            for (let i = 0; i < listaImagem.length; i++) {
                 if (imagemFilmeDeletado == listaImagem[i] && nomeFilmeDeletado == listaNome[i]) {
-                    var existe = true;
-                    var indice = i;
+                    let existe = true;
+                    let indice = i;
                     i = listaImagem.length;
                 } else {
-                    var existe = false;
+                    let existe = false;
                 }
             }
             if (existe == true) {
@@ -120,3 +95,6 @@ function removerFilme() {
     document.getElementById("nomeFilme").value = "";
 }
 
+let listaImagem = [];
+let listaNome = [];
+let localLista = "";
